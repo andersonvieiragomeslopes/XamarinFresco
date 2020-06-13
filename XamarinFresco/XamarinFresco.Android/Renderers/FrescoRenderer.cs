ccfc96d046c5575f181bb9e97eac494a8cf30c04
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-06-12 22:12:22 -0300

    Adicionar arquivos de projeto.

---
+++ XamarinFresco/XamarinFresco.Android/Renderers/FrescoRenderer.cs
@@ -0,0 +1,63 @@
+﻿using Android.Content;
+using Android.Net;
+using Android.Views;
+using Android.Widget;
+using Com.Facebook.Drawee.Backends.Pipeline;
+using Com.Facebook.Drawee.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinFresco.Controls;
+using XamarinFresco.Droid.Renderers;
+using ARelativeLayout = Android.Widget.RelativeLayout;
+
+[assembly: ExportRenderer(typeof(FrescoControls), typeof(FrescoRenderer))]
+
+namespace XamarinFresco.Droid.Renderers
+{
+    public class FrescoRenderer : ViewRenderer<FrescoControls, SimpleDraweeView>
+    {
+        Context context;
+        SimpleDraweeView draweeView;
+        ARelativeLayout relativeLayout;
+        ViewGroup mainpage;
+        public FrescoRenderer(Context context) : base(context)
+        {
+            this.context = context;
+            Fresco.Initialize(context);
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<FrescoControls> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                if (Control == null)
+                {
+
+                    draweeView = new SimpleDraweeView(context);
+                    ARelativeLayout.LayoutParams layoutParams =
+                     new ARelativeLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent);
+                    
+                    layoutParams.AddRule(LayoutRules.CenterInParent);
+                   // draweeView.LayoutParameters = layoutParams;
+                    Uri uri = Uri.Parse("https://avatars1.githubusercontent.com/u/25535951");
+
+                    draweeView.SetImageURI(uri);
+                    //relativeLayout.AddView(draweeView);
+                    SetNativeControl(draweeView);
+                    mainpage = ((ViewGroup)draweeView.Parent);
+
+                }
+               // SetSource();
+            }
+        }
+
+        private void SetSource()
+        {
+            Uri uri = Uri.Parse("https://avatars1.githubusercontent.com/u/25535951");
+
+            draweeView.SetImageURI(uri);
+
+        }
+
+    }
+}
